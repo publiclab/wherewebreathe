3af445b8839efb547601bba2ea5026764c1c139c
mmmelissa <nunes.melissa.m@gmail.com>
2014-08-13 14:10:08 -0700

    troubleshooting mobile

--- routes/index.js
+++ routes/index.js
@@ -246,11 +246,16 @@ exports.answer = function ( req, res ){
   }
   var qid = req.body.qid;
   var uid= req.user._id; 
-  var a = req.body.answer.trim();
-  console.log("answerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
-  console.log(a)
-      if (!a || a == "") {
+  var a = req.body.answer;
+    if (!a) {
       return res.send(400, "Your answer shouldnt be blank")     
+    }
+    else{
+      //cant trim if undefined
+      a = a.trim()
+      if(a == ""){
+        return res.send(400, "Your answer shouldnt be blank")  
+      }
     }
   //check that there already isnt an answer for that question/user combo (redundant, but clean data is awesome!)
   Answer.find({qid: qid, uid: uid}, function(err, existingResults){
